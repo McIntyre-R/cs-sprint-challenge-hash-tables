681118f1ad0cccf2ac4e7b2b405b79e06238145a
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-08-07 11:34:26 -0400

    first three answers

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,12 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    for e in range(length):
+        if weights[e] in cache:
+            return [e, cache[weights[e]]]
+        needed = limit - weights[e]
+        cache[needed] = e
 
+            
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,16 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
-
+    cache = {}
+    for e in tickets:
+        cache[e.source] = e.destination
+    route = [cache['NONE']]
+    i = 0
+    current = route[i]
+    while current != 'NONE':
+        route.append(cache[route[i]])
+        i += 1
+        current = cache[route[i]]
+    route.append(cache[route[i]])
     return route
+
