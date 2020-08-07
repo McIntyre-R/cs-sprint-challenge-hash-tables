00fe8bbe6d55b5b0dc1476371e574e53e51f7061
Ronald McIntyre <r.campbell.mcintyre@gmail.com>
2020-08-07 12:11:10 -0400

    problem 4/5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,7 +7,16 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
-    
+    cache = {}
+    result = []
+    for e in queries:
+        cache[e] = []
+    for k in files:
+        kspli = k.split('/')
+        if kspli[len(kspli)-1] in cache:
+            cache[kspli[len(kspli)-1]].append(k)
+    for e in cache.items():
+        result += e[1]
     return result
 
 
